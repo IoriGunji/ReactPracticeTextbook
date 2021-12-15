d6ddd2cb480aedc84b328ed3b9bb01124cd4a223
IoriGunji <iori.gunji@gmail.com>
2021-12-15 19:50:49 +0900

    Reactの基本

---
+++ Project_03/my-app/src/index.js
@@ -0,0 +1,13 @@
+import ReactDOM from "react-dom";
+import { Fragment } from "react";
+
+const App = () => {
+    return (
+        <>
+            <h1>こんにちは！</h1>
+            <p>お元気ですか？</p>
+        </>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
