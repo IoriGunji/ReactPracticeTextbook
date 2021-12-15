36c2f866f804220fcdadb856a29168edc8381fbf
IoriGunji <iori.gunji@gmail.com>
2021-12-15 11:41:48 +0900

    DOMの追加

---
+++ Project_01/src/index.js
@@ -0,0 +1,16 @@
+console.log("test");
+
+const title = document.getElementById("title");
+console.log(title);
+
+const title2 = document.querySelector("#title");
+console.log(title2);
+
+const continers = document.getElementsByClassName("container");
+console.log(continers);
+
+const container = document.querySelector(".container");
+console.log(continers);
+
+const containers = document.querySelectorAll(".container");
+console.log(containers);
